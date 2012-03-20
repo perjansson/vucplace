25ecf7f4eda0501b8d31488aa5278d728b1aa770
Per Jansson <pichdude@gmail.com>
2012-03-20 22:38:15 +0100

    Supressing warning message.

--- VucPlace/src/nu/pich/vucplace/client/batch/rpc/BatchCallback.java
+++ VucPlace/src/nu/pich/vucplace/client/batch/rpc/BatchCallback.java
@@ -6,6 +6,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class BatchCallback implements AsyncCallback<BatchResult> {
 
+	@SuppressWarnings("unused")
 	private final Handler handler;
 
 	public BatchCallback(Handler handler) {
